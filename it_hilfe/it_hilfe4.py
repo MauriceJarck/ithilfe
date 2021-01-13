431dd1e92a74e5f464ca12ec6e0cbb50c660f78d
Maurice Jarck <maurice.jarck@accenture.com>
2021-01-13 10:00:56 +0100

    restructured get_available() => no need to start allover again if invalid input + help()/cancel functionality, register() restructured for same reason, changeparam() restructured + print current value, restructured main() to fit changes mentioned above, fitted tests

--- it_hilfe/it_hilfe4.py
+++ it_hilfe/it_hilfe4.py
@@ -41,11 +41,24 @@ options = ["search by username", "register new", "view all", "change parameter",
 valid_devices = [WindowsLapTop, WindowsWorkStation, Macbook]  # to add more classes add class here
 
 def get_available(optionslist):
-    a = int(input("\n".join(["{}:{}".format(i + 1, x) if type(x) == str else "{}:{}".format(i + 1, x.__name__) for i, x in enumerate(optionslist)])+"\n>"))
-    if a != 0 and a <= len(optionslist):
-        return optionslist[a-1]
+    try:
+        if optionslist[len(optionslist)-1] != "help":
+            optionslist = optionslist+["help"]
+        a = int(input("\n".join(["{}:{}".format(i + 1, x) if type(x) == str else "{}:{}".format(i + 1, x.__name__) for i, x in enumerate(optionslist)])+"\n>"))
+    except(ValueError):
+        print('\033[91m' + "\nindex can only be int, retry:\n" + '\033[0m')
+        return str(get_available(optionslist))
+    except KeyboardInterrupt:
+        raise KeyboardInterrupt
     else:
-        raise IndexError
+        if a <= len(optionslist) and a >= 1:
+            if optionslist[a-1] == "help":
+                print("\nwhen running in terminal: press ctrl+c+return to exit current menue => back to  main\n")
+                return get_available(optionslist)
+            return optionslist[a-1]
+        else:
+            print('\033[91m' + "\nindex must belong to range of available choices, retry:\n" + '\033[0m')
+            return get_available(optionslist)
 
 
 def view():
@@ -54,58 +67,63 @@ def view():
 
 
 def register():
-    newdevicetype = get_available(valid_devices)
     newdevicename = input("enter devicename \nalready taken: {}\n>".format(', '.join([str(x) for x in list(registered_devices.keys())]) or "None"))
-    new = newdevicetype(newdevicename, input("enter username \n>"))
     if newdevicename not in registered_devices.keys():
+        newdevicetype = get_available(valid_devices)
+        new = newdevicetype(newdevicename, input("enter username \n>"))
         if len(newdevicetype.expected_OS) > 1:
             new.OS = get_available(newdevicetype.expected_OS)
         registered_devices[new.name] = new
         return new
+    else:
+        print('\033[91m' + "already taken dev name\n" + '\033[0m')
+        return register()
 
 
 def search(username):
-    return [x for x in registered_devices.values() if x.user == username]
+    return [x for x in registered_devices.values() if x.user == username] or ['\033[91m' + "no match found\n" + '\033[0m']
 
 
-def change_param(devicename, paramtype):
-    a = registered_devices.get(devicename)
-    if paramtype == "OS":
-        setattr(a, paramtype, get_available(a.expected_OS))
-    else:
-        setattr(a, paramtype, input("enter new parameter\n>"))
-    return a
-
+def change_param():
+    try:
+        devicename = input("existent devicenames: {}\nenter devicename you want to change \n> ".format(", ".join([str(x) for x in list(registered_devices.keys())])))
+        paramtype= get_available(registered_devices[devicename].visible_attr)
+        a = registered_devices.get(devicename)
+        print("current val:", getattr(a, paramtype))
+        if paramtype == "OS":
+            setattr(a, paramtype, get_available(a.expected_OS))
+        else:
+            setattr(a, paramtype, input("enter new parameter\n>"))
+        return a
+    except KeyError:
+        print('\033[91m' + "\nnot found, retry:\n" + '\033[0m')
+        return change_param()
 
 def main():  # to extend menu functionality add here
     print("welcome to IT service\ntype no. of what you wish to do\n")
     while True:
         try:
             w = get_available(options)
-            if w == "search by username":
-                if len(registered_devices) != 0:
-                    print("\n"+"".join([str(x) for x in search(input("enter username you wish to search for\n>"))]), "\n" or '\033[91m' + "no match found\n" + '\033[0m')
-                else:
-                    print('\033[91m' + "no device registered yet\n" + '\033[0m')
-            if w == "register new":
-                a = register()
-                if a is None:
-                    print('\033[91m' + "already taken dev name\n" + '\033[0m')
-                else:
-                    print("\n" + str(a) + "\n")
-            elif w == "view all":
-                print("\n".join([str(x) for x in view()]),"\n" or '\033[91m' + "no device registered yet\n" + '\033[0m')
-            elif w == "change parameter":
-                if len(registered_devices) != 0:
-                    name = input("existent devicenames: {}\nenter devicename you want to change \n> ".format(", ".join(list(registered_devices.keys()))))
-                    paramtype = get_available(registered_devices[name].visible_attr)
-                    print("\n" + str(change_param(name, paramtype)) + "\n")
-                else:
-                    print('\033[91m' + "no device registered yet\n" + '\033[0m')
-            elif w == "quit program":
-                quit(0)
-        except(ValueError, IndexError, AttributeError, KeyError):
-            print('\033[91m' + "index can only be int and must belong to range of available choices\n" + '\033[0m')
+            if len(registered_devices) >= 0:
+                if w == "register new":
+                    print("\n" + str(register()) + "\n")
+                elif w == "quit program":
+                    quit(0)
+
+            if len(registered_devices) != 0:
+                if w == "search by username":
+                    print("\n"+"\n".join([str(x) for x in search(input(f"enter username to search for, available: {', '.join([str(x) for x in set([x.user for x in list(registered_devices.values())])])}\n>"))]))
+                elif w == "view all":
+                    print("\n".join([str(x) for x in view()]), "\n" or '\033[91m' + "no device registered yet\n" + '\033[0m')
+                elif w == "change parameter":
+                    print("\n" + str(change_param()) + "\n")
+            else:
+                print('\033[91m' + "no device registered yet\n" + '\033[0m')
+
+
+        except(KeyboardInterrupt):
+            print("\nback to the roots\n" )
+
 
 if __name__ == "__main__":
     main()
--- tests/it_hilfe4_test.py
+++ tests/it_hilfe4_test.py
@@ -1,5 +1,8 @@
 from it_hilfe import it_hilfe4
 from pytest import fixture, raises, mark
+from sys import setrecursionlimit
+
+setrecursionlimit(68)  # avoid too much unnecessary recursions
 
 
 @fixture(scope="function")
@@ -30,7 +33,6 @@ def test_WindowsLapTop():
     assert str(dev1) == "1, Maurice, None, WindowsLapTop, largerbattery: True, upgradedCPU: False"
 
 
-
 def test_Macbook():
     dev1 = it_hilfe4.Macbook(2, "Maurice")
     assert dev1.name == 2
@@ -45,12 +47,24 @@ def test_WinWorkStation():
     assert str(dev1) == "3, Maurice, None, WindowsWorkStation"
 
 
-def test_getAvialable(monkeypatch):
+def test_getAvialable(monkeypatch, capsys):
     monkeypatch.setattr("builtins.input", lambda _: 1)
     assert it_hilfe4.get_available(["search by username", "register new", "view all"]) == "search by username"
 
-    with raises(IndexError):
-        monkeypatch.setattr("builtins.input", lambda _: 4)
+    with raises(RecursionError):  # tests is input is wrong val gets invoked
+        monkeypatch.setattr("builtins.input", lambda _: ["a"].pop(0))
+        it_hilfe4.get_available(["search by username", "register new", "view all"])
+
+    with raises(RecursionError):  # tests is input is help recursion gets invoked
+        monkeypatch.setattr("builtins.input", lambda _: [4].pop(0))
+        it_hilfe4.get_available(["search by username", "register new", "view all"])
+
+    with raises(RecursionError):  # tests is input is over valid index val
+        monkeypatch.setattr("builtins.input", lambda _: [5].pop(0))
+        it_hilfe4.get_available(["search by username", "register new", "view all"])
+
+    with raises(RecursionError):  # tests is input is under valid index val
+        monkeypatch.setattr("builtins.input", lambda _: [0].pop(0))
         it_hilfe4.get_available(["search by username", "register new", "view all"])
 
 
@@ -58,23 +72,25 @@ def test_getAvialable(monkeypatch):
                   [([1, 1, "maurice", 1], [1, "maurice", "Win10", "WindowsLapTop", True, False, 1234]),
                    ([2, 2, "Peter", 1], [2, "Peter", "Win10", "WindowsWorkStation"]),
                    ([3, 3, "Heinz"], [3, "Heinz", "MacOS", "Macbook"]),
-                   ([1, 1, "maurice", 1], [])])
+                   ])
 def test_register(monkeypatch, test_input, expected):
     monkeypatch.setattr("builtins.input", lambda _x: test_input.pop(0))
     func = it_hilfe4.register()
-    if func is not None:
-        assert func.name == expected[0]
-        assert func.user == expected[1]
-        assert func.OS == expected[2]
-        assert func.__class__.__name__ == expected[3]
-        try:
-            assert func.largerBattery == expected[4]
-            assert func.upgradedCPU == expected[5]
-            assert func.bitlockkey == expected[6]
-        except AttributeError:
-            pass
-    else:
-        assert func is None
+
+    assert func.name == expected[0]
+    assert func.user == expected[1]
+    assert func.OS == expected[2]
+    assert func.__class__.__name__ == expected[3]
+    try:
+        assert func.largerBattery == expected[4]
+        assert func.upgradedCPU == expected[5]
+        assert func.bitlockkey == expected[6]
+    except AttributeError:
+        pass
+
+    with raises(RecursionError):  # test if a already existing registry provokes register() to call itself
+        monkeypatch.setattr("builtins.input", lambda _: [1, 1, "maurice"].pop(0))
+        it_hilfe4.register()
 
 
 def test_view():
@@ -96,15 +112,17 @@ def test_view():
 
     assert it_hilfe4.view() == []
 
+
 @mark.parametrize("test_input,expected", [
-    ([1, "user", "peter"], [1, "peter", "Win7", "WindowsLapTop", True, False]),
-    ([1, "OS", 2], [1, "Maurice", "Win7", "WindowsLapTop", True, False]),
-    ([1, "largerBattery", False], [1, "Maurice", "Win7", "WindowsLapTop", False, False]),
-    ([1, "upgradedCPU", True], [1, "Maurice", "Win7", "WindowsLapTop", True, True])])
+    ([1, 1, "peter"], [1, "peter", "Win7", "WindowsLapTop", True, False]),
+    ([1, 2, 2], [1, "Maurice", "Win7", "WindowsLapTop", True, False]),
+    ([1, 3, False], [1, "Maurice", "Win7", "WindowsLapTop", False, False]),
+    ([1, 4, True], [1, "Maurice", "Win7", "WindowsLapTop", True, True])
+])
 def test_change(test_input, expected, create_single_register, monkeypatch):
 
-    monkeypatch.setattr("builtins.input", lambda _x: test_input[2])
-    func = it_hilfe4.change_param(test_input[0], test_input[1])
+    monkeypatch.setattr("builtins.input", lambda _x: test_input.pop(0))
+    func = it_hilfe4.change_param()
 
     assert func.name == expected[0]
     assert func.user == expected[1]
@@ -121,10 +139,10 @@ def test_search(create_single_register):
     assert func[0].OS == "Win7"
     assert func[0].__class__.__name__ == "WindowsLapTop"
     assert func[0].largerBattery is True
-    assert  func[0].upgradedCPU is False
+    assert func[0].upgradedCPU is False
 
-    assert it_hilfe4.search("Heinz") == []
+    assert it_hilfe4.search("Heinz") == ['\x1b[91mno match found\n\x1b[0m']
 
     it_hilfe4.registered_devices.clear()
 
-    assert it_hilfe4.search("maurice") == []
+    assert it_hilfe4.search("maurice") == ['\x1b[91mno match found\n\x1b[0m']
