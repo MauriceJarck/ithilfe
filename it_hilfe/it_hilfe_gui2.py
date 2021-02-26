0fca8418649aa50ebde3db9b789bc521a65c4be4
Maurice Jarck <maurice.jarck@accenture.com>
2021-02-26 12:01:29 +0100

    tests update

--- it_hilfe/it_hilfe_gui2.py
+++ it_hilfe/it_hilfe_gui2.py
@@ -122,7 +122,7 @@ class MainWindow(QMainWindow):
 
             """
         super(MainWindow, self).__init__()
-        loadUi("../tests/mainWindow.ui", self)
+        loadUi("mainWindow.ui", self)
         # etc
         self.fname = None
         self.dir = None
@@ -393,7 +393,7 @@ class StartScreen(QWidget):
         loads interface aka .ui file"""
 
         super(StartScreen, self).__init__()
-        loadUi("../tests/startscreen.ui", self)
+        loadUi("startscreen.ui", self)
         self.show()
         self.timer = QtCore.QTimer()
         self.timer.singleShot(1500, self.on_elapsed)
