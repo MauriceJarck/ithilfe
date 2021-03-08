1603638c4c6f6b3e11de7b66d49480b4e2480afe
Maurice Jarck <maurice.jarck@accenture.com>
2021-03-08 13:31:42 +0100

    update coverage badge

--- .github/update_cov_badge.py
+++ .github/update_cov_badge.py
@@ -3,14 +3,15 @@ import xml.dom.minidom as md
 cov_file = md.parse("../tests/coverage.xml")
 for y in cov_file.getElementsByTagName("package"):
     percentage = round(float(y.getAttribute("line-rate")),2)*100
-    print(percentage)
 
 badge_file = md.parse("cov_badge.svg")
 for x in badge_file.getElementsByTagName("text"):
     if str(x.firstChild.nodeValue).endswith("%"):
-        print(x.firstChild.nodeValue)
+        old = x.firstChild.nodeValue
         x.firstChild.nodeValue = str(percentage)+"%"
-        print(x.firstChild.nodeValue)
+        new = x.firstChild.nodeValue
+        if old != new:
+            print(f"old: {old}, new: {new}")
 
 with open("../.github/cov_badge.svg", "w") as fs:
     fs.write(badge_file.toxml())
