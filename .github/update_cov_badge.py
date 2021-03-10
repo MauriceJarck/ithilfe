ff9ea128bfa7a251c2235d22b3079ba8d289011c
Maurice Jarck <maurice.jarck@accenture.com>
2021-03-10 17:09:17 +0100

    set cov val as env var

--- .github/update_cov_badge.py
+++ .github/update_cov_badge.py
@@ -1,18 +1,10 @@
 import xml.dom.minidom as md
+import os
 
 cov_file = md.parse("../tests/coverage.xml")
 for y in cov_file.getElementsByTagName("package"):
     percentage = round(float(y.getAttribute("line-rate")),2)*100
 
-badge_file = md.parse("cov_badge.svg")
-for x in badge_file.getElementsByTagName("text"):
-    if str(x.firstChild.nodeValue).endswith("%"):
-        old = x.firstChild.nodeValue
-        x.firstChild.nodeValue = str(percentage)+"%"
-        new = x.firstChild.nodeValue
-        if old != new:
-            print(f"old: {old}, new: {new}")
+os.environ["COVERAGE"] = str(percentage)
+print(os.environ["COVERAGE"])
 
-with open("../.github/cov_badge.svg", "w") as fs:
-    fs.write(badge_file.toxml())
-    fs.close()
