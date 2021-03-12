c7adfb1668b7dc3154bcd4eddb9af286ae1cf012
Maurice Jarck <maurice.jarck@accenture.com>
2021-03-12 10:46:05 +0100

    update update_cov_badge.py and .gitignore

--- .github/update_cov_badge.py
+++ .github/update_cov_badge.py
@@ -4,8 +4,9 @@ import os
 cov_file = md.parse("../tests/coverage.xml")
 for y in cov_file.getElementsByTagName("package"):
     percentage = round(float(y.getAttribute("line-rate")),2)*100
+print(os.environ)
 
 os.environ["COVERAGE"] = str(percentage)
 
-print(os.environ["COVERAGE"])
+print(os.environ)
 
