7f3d33d26211a1dbbecf9176575d09dc7f4550e4
Maurice Jarck <maurice.jarck@accenture.com>
2021-03-15 09:29:12 +0100

    output env

--- .github/update_cov_badge.py
+++ .github/update_cov_badge.py
@@ -6,6 +6,6 @@ for y in cov_file.getElementsByTagName("package"):
     percentage = round(float(y.getAttribute("line-rate")),2)*100
 
 os.system(f'cmd /k "set TEST_COVERAGE={percentage}"')
-print(os.system('cmd /k "echo %TEST_COVERAGE%"'))
-print(os.environ["TEST_COVERAGE"])
+# print(os.system('cmd /k "echo %TEST_COVERAGE%"'))
+# print(os.environ["TEST_COVERAGE"])
 
