6f13767dafba0e0cf4e719815ade31abdd2583ed
Maurice Jarck <maurice.jarck@accenture.com>
2021-03-15 09:26:05 +0100

    output env

--- .github/update_cov_badge.py
+++ .github/update_cov_badge.py
@@ -4,9 +4,8 @@ import os
 cov_file = md.parse("../tests/coverage.xml")
 for y in cov_file.getElementsByTagName("package"):
     percentage = round(float(y.getAttribute("line-rate")),2)*100
-# print(os.environ["TEST_COVERAGE "])
 
-os.environ["TEST_COVERAGE "] = str(percentage)
-
-print(os.environ["TEST_COVERAGE "])
+os.system(f'cmd /k "set TEST_COVERAGE={percentage}"')
+print(os.system('cmd /k "echo %TEST_COVERAGE%"'))
+print(os.environ["TEST_COVERAGE"])
 
