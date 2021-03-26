740d1707c968c631e3a9bc4bf35af5e8ddd6f83b
JackWolf24 <73477885+JackWolf24@users.noreply.github.com>
2021-03-26 11:52:14 +0100

    Update update_cov_badge.py

--- .github/update_cov_badge.py
+++ .github/update_cov_badge.py
@@ -1,6 +1,6 @@
 import xml.dom.minidom as md
 
-cov_file = md.parse("../tests/coverage2.xml")
+cov_file = md.parse("../coverage.xml")
 for y in cov_file.getElementsByTagName("package"):
     percentage = round(float(y.getAttribute("line-rate")), 2)*100
 
